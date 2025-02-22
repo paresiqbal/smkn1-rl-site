fe8d9a59d4a7f1a395d0b425cfc28382299a279e
paresiqbal <thecrowdel@gmail.com>
2025-02-22 15:32:00 +0700

    initialize project structure with essential files and configurations

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMK Negeri 1 Rejang Lebong</title>
+</head>
+
+<body>
+    <div>
+        <h1>Wellcome to SMK Negeri 1 Rejang Lebong</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, voluptates?</p>
+    </div>
+</body>
+
+</html>
