dbc582390bc05a07622bb693ae1d59d054798165
paresiqbal <thecrowdel@gmail.com>
2025-02-22 22:28:03 +0700

    add community route group and initialize community routes file

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// auth
+Route::get('/register', [])->name('show.register');
+Route::get('/login', [])->name('show.login');
+
 
 Route::name("admin.")->domain("admin." . env("APP_URL"))->group(function () {
     require __DIR__ . '/web/admin.php';
 });
 
-Route::name("users.")->domain("freelancer." . env("APP_URL"))->group(function () {
+Route::name("community.")->domain("community." . env("APP_URL"))->group(function () {
     require __DIR__ . '/web/user.php';
-});
-
-Route::get('/', function () {
-    return view('welcome');
 });
