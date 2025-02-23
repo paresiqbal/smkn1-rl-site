537407345b6fae34eacd7b7fb203f41712fcfca8
paresiqbal <thecrowdel@gmail.com>
2025-02-23 23:03:16 +0700

    feat: refactor login view to use Blade templating and enhance styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
+@section('title', 'Login')
 
-<body>
-    <div class="container">
-        <h2>Login</h2>
+@section('content')
+    <div
+        class="container border-4 border-black p-6 w-full max-w-md shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] transition-all dark:border-white dark:shadow-[6px_6px_0px_0px_rgba(255,255,255,1)] bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
+        <h2 class="text-2xl font-bold text-center mb-6">Login</h2>
         <form method="POST" action="{{ route('show.login') }}">
             @csrf
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" class="form-control" required>
+            <div class="form-group mb-4">
+                <label for="email" class="block mb-2">Email:</label>
+                <input type="email" id="email" name="email"
+                    class="w-full border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-yellow-300 active:shadow-[2px_2px_0px_rgba(0,0,0,1)]"
+                    required>
             </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" class="form-control" required>
+            <div class="form-group mb-4">
+                <label for="password" class="block mb-2">Password:</label>
+                <input type="password" id="password" name="password"
+                    class="w-full border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-yellow-300 active:shadow-[2px_2px_0px_rgba(0,0,0,1)]"
+                    required>
             </div>
-            <button type="submit" class="btn btn-primary">Login</button>
+            <button type="submit"
+                class="h-12 w-full text-black border-2 border-black p-2.5 bg-yellow-300 shadow-[4px_4px_0px_rgba(0,0,0,1)] hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition">
+                Login
+            </button>
         </form>
     </div>
-</body>
-
-</html>
+@endsection
