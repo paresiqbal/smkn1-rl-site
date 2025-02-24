51290dd428dc6f857541b776f238fd51fb31be21
paresiqbal <thecrowdel@gmail.com>
2025-02-24 11:56:24 +0700

    feat: add banner component and school image to enhance homepage layout

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,17 @@
+<div class="pt-10 flex flex-col lg:flex-row items-center gap-4">
+    <!-- Left Section (Text) -->
+    <div class="lg:w-1/2 space-y-4">
+        <h1 class="text-4xl font-bold">SMK Negeri 1 Rejang Lebong</h1>
+        <p class="text-lg text-balance text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
+            dicta dolores dignissimos similique harum obcaecati eaque magnam libero perferendis laboriosam!</p>
+        <div class="flex gap-4">
+            <button class="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700">Pelajari Sekarang</button>
+            <button class="bg-green-600 text-white px-6 py-2 rounded-lg hover:bg-green-700">Daftar Sekarang</button>
+        </div>
+    </div>
+
+    <!-- Right Section (Image) -->
+    <div class="lg:w-1/2">
+        <img src="{{ asset('assets/school.jpg') }}" alt="sekolah" class="w-full rounded-lg shadow-lg">
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,6 @@
 
 @section('content')
     <div>
-        <h1>Welcome to SMK Negeri 1 Rejang Lebong</h1>
-        <p class="text-balance">This design style emphasizes bold shapes, high contrast, and raw aesthetics. It's
-            characterized by stark borders,
-            offset shadows, and a deliberately unpolished look.
-        </p>
+        @include('components.banner')
     </div>
 @endsection
