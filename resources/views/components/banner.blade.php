b43ef383999b925e4be185c173946d26b83f65d3
paresiqbal <thecrowdel@gmail.com>
2025-02-24 15:14:17 +0700

    feat: enhance banner and detail components with improved layout and additional content

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,5 +1,4 @@
-<div class="pt-8 lg:pt-24 flex flex-col lg:flex-row items-center gap-6">
-    <!-- Left Section (Text) -->
+<div class="pt-8 lg:pt-24 flex flex-col lg:flex-row items-center gap-6 selection:bg-red-300">
     <div class="lg:w-1/2 space-y-8">
         <h1 class="text-4xl lg:text-7xl font-bold">SMK Negeri 1 Rejang Lebong</h1>
         <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
@@ -7,14 +6,13 @@
         <div class="flex gap-4">
             <button
                 class="h-12 w-full text-black border-2 border-black dark:border-white p-2.5 bg-yellow-300 shadow-[4px_4px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)] hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition">Pelajari
-                Sekarang</button>
+            </button>
             <button
                 class="h-12 w-full text-black border-2 border-black dark:border-white p-2.5 bg-red-400 shadow-[4px_4px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)] hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition">Daftar
                 Sekarang</button>
         </div>
     </div>
 
-    <!-- Right Section (Image) -->
     <div class="lg:w-1/2">
         <img src="{{ asset('assets/school.jpg') }}" alt="sekolah"
             class="w-full shadow-[6px_6px_0px_rgba(0,0,0,1)] dark:border-white dark:shadow-[6px_6px_0px_0px_rgba(255,255,255,1)]">
--- resources/views/components/detail.blade.php
+++ resources/views/components/detail.blade.php
@@ -1,31 +1,77 @@
-<div class="pt-8 lg:pt-24 flex flex-col lg:flex-row items-center gap-6">
-    <div class="text-center">
-        <h2 class="text-2xl font-bold">SMK Negeri 1 Rejang Lebong</h2>
-        <p>Membangun generasi muda cerdas dan berakhlak.</p>
-    </div>
-    <div>
-        <div class="flex gap-4">
-            <button
-                class="h-12 w-full text-black border-2 border-black dark:border-white p-2.5 bg-yellow-300 shadow-[4px_4px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)] hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition">Brosur
-                PPDB
-            </button>
-            <button
-                class="h-12 w-full text-black border-2 border-black dark:border-white p-2.5 bg-yellow-300 shadow-[4px_4px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)] hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition">Fasilitas
-            </button>
+<div class="pt-8 lg:pt-24 overflow-hidden">
+    <div class="flex flex-col lg:flex-row items-center gap-6 lg:justify-between">
+        <div class="flex flex-col gap-8 justify-center">
+            <div class="text-center">
+                <h2 class="mb-2 text-2xl font-bold md:text-3xl">SMK Negeri 1 Rejang Lebong</h2>
+                <p class="pt-2">Membangun generasi muda cerdas dan berakhlak.</p>
+            </div>
+            <div class="flex gap-4">
+                <button
+                    class="h-12 w-full text-black border-2 border-black dark:border-white p-2.5 bg-yellow-300 shadow-[4px_4px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)] hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition">Brosur
+                    PPDB
+                </button>
+                <button
+                    class="h-12 w-full text-black border-2 border-black dark:border-white p-2.5 bg-yellow-300 shadow-[4px_4px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)] hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition">Fasilitas
+                </button>
+            </div>
         </div>
         <div>
-            <div>
-                <p>B</p>
-                <p>Akreditasi</p>
-            </div>
-            <div>
-                <p>B</p>
-                <p>Akreditasi</p>
-            </div>
-            <div>
-                <p>B</p>
-                <p>Akreditasi</p>
+            <div class="grid w-full grid-cols-3 gap-4 text-center md:w-auto">
+                <div
+                    class="flex flex-col items-center p-4 md:p-6 border-2 bg-red-500 border-black dark:border-white shadow-[4px_4px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)]">
+                    <p class="text-2xl font-bold md:text-3xl">B</p>
+                    <p class="text-sm md:text-base">Akreditasi</p>
+                </div>
+                <div
+                    class="flex flex-col items-center p-4 md:p-6 border-2 bg-red-500 border-black dark:border-white shadow-[4px_4px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)]">
+                    <p class="text-2xl font-bold md:text-3xl">200+</p>
+                    <p class="text-sm md:text-base">Siswa</p>
+                </div>
+                <div
+                    class="flex flex-col items-center p-4 md:p-6 border-2 bg-red-500 border-black dark:border-white shadow-[4px_4px_0px_rgba(0,0,0,1)] dark:shadow-[4px_4px_0px_0px_rgba(255,255,255,1)]">
+                    <p class="text-2xl font-bold md:text-3xl">7</p>
+                    <p class="text-sm md:text-base">Jurusan</p>
+                </div>
             </div>
         </div>
+    </div>
+    <div class="mx-auto my-6 pt-2 lg:pt-10 flex flex-col items-center justify-center gap-4 px-2 md:px-0">
+        <section
+            class="grid grid-cols-2 gap-y-4 space-x-2 text-center md:flex md:justify-between md:space-x-20 md:text-left">
+            <a href="/major/tkj"
+                class="cursor-pointer text-sm decoration-primary decoration-2 underline-offset-4 hover:underline md:text-base">
+                Teknik Komputer & Jaringan
+            </a>
+            <a href="/major/tkj"
+                class="cursor-pointer text-sm decoration-primary decoration-2 underline-offset-4 hover:underline md:text-base">
+                Teknik Kendaraan Ringan
+            </a>
+            <a href="/major/tkj"
+                class="cursor-pointer text-sm decoration-primary decoration-2 underline-offset-4 hover:underline md:text-base">
+                Teknik Bisnis Sepeda Motor
+            </a>
+            <a href="/major/tkj"
+                class="cursor-pointer text-sm decoration-primary decoration-2 underline-offset-4 hover:underline md:text-base">
+                Teknik Elektronika
+            </a>
+        </section>
+        <section class="grid grid-cols-2 gap-y-4 text-center md:flex md:justify-between md:space-x-11 md:text-left">
+            <a href="/major/tkj"
+                class="cursor-pointer text-sm decoration-primary decoration-2 underline-offset-4 hover:underline md:text-base">
+                Teknik Desain Pemodelan
+            </a>
+            <a href="/major/tkj"
+                class="cursor-pointer text-sm decoration-primary decoration-2 underline-offset-4 hover:underline md:text-base">
+                Teknik Instalasi Tenaga Listrik
+            </a>
+            <a href="/major/tkj"
+                class="cursor-pointer text-sm decoration-primary decoration-2 underline-offset-4 hover:underline md:text-base">
+                Teknik Pengelasan
+            </a>
+            <a href="/major/tkj"
+                class="cursor-pointer text-sm decoration-primary decoration-2 underline-offset-4 hover:underline md:text-base">
+                Teknik Mesin
+            </a>
+        </section>
     </div>
 </div>
