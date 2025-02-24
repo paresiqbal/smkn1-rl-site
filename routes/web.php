6da4bed41eab0d92cdd56f0858490b8f475f398a
paresiqbal <thecrowdel@gmail.com>
2025-02-24 10:04:50 +0700

    feat: apply guest middleware to authentication routes for improved access control

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,14 @@ Route::get('/', function () {
 });
 
 // auth
-Route::get('/register-admin', [AuthController::class, 'showRegisterAdmin'])->name('show.register-admin');
-Route::post('/register-admin', [AuthController::class, 'registerAdmin'])->name('register-admin');
-Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-Route::post('/register', [AuthController::class, 'registerUser'])->name('register');
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
+    Route::get('/register-admin', 'showRegisterAdmin')->name('show.register-admin');
+    Route::post('/register-admin', 'registerAdmin')->name('register-admin');
+    Route::get('/register',  'showRegister')->name('show.register');
+    Route::post('/register',  'registerUser')->name('register');
+    Route::get('/login',  'showLogin')->name('show.login');
+    Route::post('/login',  'login')->name('login');
+});
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::name("admin.")->domain("admin." . env("APP_URL"))->group(function () {
