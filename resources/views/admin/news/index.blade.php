d29f7354ab3dc950d66d6881881517f5eb202854
paresiqbal <thecrowdel@gmail.com>
2025-03-01 19:36:12 +0700

    refactor: clean up Quill editor toolbar options for improved readability

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -71,7 +71,7 @@
 
     <script>
         const toolbarOptions = [
-            ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+            ['bold', 'italic', 'underline', 'strike'],
             ['blockquote', 'code-block'],
             ['link', 'image', 'video', 'formula'],
 
@@ -79,7 +79,7 @@
                 'header': 1
             }, {
                 'header': 2
-            }], // custom button values
+            }],
             [{
                 'list': 'ordered'
             }, {
@@ -91,19 +91,19 @@
                 'script': 'sub'
             }, {
                 'script': 'super'
-            }], // superscript/subscript
+            }],
             [{
                 'indent': '-1'
             }, {
                 'indent': '+1'
-            }], // outdent/indent
+            }],
             [{
                 'direction': 'rtl'
-            }], // text direction
+            }],
 
             [{
                 'size': ['small', false, 'large', 'huge']
-            }], // custom dropdown
+            }],
             [{
                 'header': [1, 2, 3, 4, 5, 6, false]
             }],
@@ -112,13 +112,13 @@
                 'color': []
             }, {
                 'background': []
-            }], // dropdown with defaults from theme
+            }],
 
             [{
                 'align': []
             }],
 
-            ['clean'] // remove formatting button
+            ['clean']
         ];
 
         // Initialize Quill
